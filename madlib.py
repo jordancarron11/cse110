e81638d713c322337b1a6d8c66948ed8f9725aee
jordancarron11 <jordancarron11@gmail.com>
2022-12-31 15:56:09 -0700

    cool

--- madlib.py
+++ madlib.py
@@ -1,14 +1,14 @@
 adjective = input("pick an adjective: ")
 animal = input("pick an animal: ")
-verb1 = input("pick a verb: ")
-verb2 = input("pick another verb: ")
-verb3 = input("pick a final verb: ")
-exclamation = input("pick an explamation: ")
+verb1 = input("pick 1st verb: ")
+verb2 = input("pick 2nd verb: ")
+verb3 = input("pick 3rd verb: ")
+exclamation = input("pick an exclamation: ")
 
 story = f'''The other day, I was really in trouble. It all started when I saw a very 
-{adjective} {animal} {verb1} down the hallway. '{exclamation}!' I yelled. 
-But all I could think to do was to {verb2} over and over. 
-Miraculously,that caused it to stop, 
-but not before it tried to {verb3} right in front of my family.'''
+{adjective} {animal} {verb1} down the hallway. '{exclamation}!' I yelled. But all 
+I could think to do was to {verb2} over and over. Miraculously, 
+that caused it to stop, but not before it tried to {verb3} 
+right in front of my family.'''
 
 print(story)
