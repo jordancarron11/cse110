4536042f0c95e8227d4d1f37e39d397810c6b658
jordancarron11 <jordancarron11@gmail.com>
2022-12-31 15:24:13 -0700

    added

--- checkpoint2.py
+++ checkpoint2.py
@@ -1,4 +1,4 @@
 fname = input("What is your first name? ")
 lname = input("What is your last name? ")
 
-print(f"your name is {lname}, {fname}{lname}")
+print(f"your name is {lname}, {fname} {lname}")
