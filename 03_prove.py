25c11bbadbbe2fd97a093a2448a61d70197f43d3
jordancarron11 <jordancarron11@gmail.com>
2023-01-09 19:02:53 -0700

    add

--- 03_prove.py
+++ 03_prove.py
@@ -8,18 +8,20 @@ total_child_cost = child_meal_cost * num_children
 total_adult_cost = adult_meal_cost * num_adult
 
 subtotal = total_adult_cost + total_child_cost
-tax = (tax_rate * total) / 100
+tax = (tax_rate * subtotal) / 100
 
 total = subtotal + tax
 
-print(f"Subtotal: {total}")
-print(f"Sales Tax: {tax}")
-print(f"Total: {total}\n")
+print(f"Subtotal: ${total:.2f}")
+print(f"Sales Tax: ${tax:.2f}")
+print(f"Total: ${total:.2f}\n")
 
-payment_amount = float(input("what is the payment amount? "))
+payment_amount = float(input("what is the payment amount? $"))
 change = payment_amount - total
 
 if change < 0:
-    print("Sorry that does not cover the bill please add more")
+    print("Sorry that does not cover the bill please add more to the payment amount ")
+elif change == 0:
+    print("There is no change that is the exact amount ")
 else:
-    print(f"Change: {change}")
+    print(f"Change: ${change:.2f}")
