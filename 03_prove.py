5d80663d09898cc6b0dd0388e536d0c8ae8fb6e8
jordancarron11 <jordancarron11@gmail.com>
2023-01-09 18:47:57 -0700

    ADD

--- 03_prove.py
+++ 03_prove.py
@@ -7,17 +7,17 @@ print("\n")
 total_child_cost = child_meal_cost * num_children
 total_adult_cost = adult_meal_cost * num_adult
 
-total = total_adult_cost + total_child_cost
+subtotal = total_adult_cost + total_child_cost
 tax = (tax_rate * total) / 100
 
-total_w_tax = total + tax
+total = subtotal + tax
 
 print(f"Subtotal: {total}")
 print(f"Sales Tax: {tax}")
-print(f"Total: {total_w_tax}\n")
+print(f"Total: {total}\n")
 
 payment_amount = float(input("what is the payment amount? "))
-change = payment_amount - total_w_tax
+change = payment_amount - total
 
 if change < 0:
     print("Sorry that does not cover the bill please add more")
