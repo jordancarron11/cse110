d3916646dc9c2841ddc877957898592d3d2195e8
jordancarron11 <jordancarron11@gmail.com>
2023-01-13 19:36:10 -0700

    add

---
+++ Checkpoint05.py
@@ -0,0 +1,11 @@
+num1 = input("First number: ")
+num2 = input("Second number: ")
+
+if num1 > num2:
+    print("number 1 is greater than number 2")
+
+elif num1 == num2:
+    print("both numbers are the same!")
+
+else:
+    print("number 2 is greater than number 1 ")
