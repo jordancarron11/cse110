b4f1cb9cc00606e473f282a6bec9af30af220452
jordancarron11 <jordancarron11@gmail.com>
2023-01-13 19:41:51 -0700

    add

--- Checkpoint05.py
+++ Checkpoint05.py
@@ -3,9 +3,21 @@ num2 = input("Second number: ")
 
 if num1 > num2:
     print("number 1 is greater than number 2")
-
-elif num1 == num2:
-    print("both numbers are the same!")
-
 else:
+    print("number 1 is not greater than number 2")
+
+if num1 == num2:
+    print("both numbers are the same!")
+else:
+    print("both numbers are not the same")
+
+if num2 > num1:
     print("number 2 is greater than number 1 ")
+else:
+    print("number 2 is not greater than number 1 ")
+
+favAnimal = input("What is your favorite animal? ")
+if favAnimal == "turtle":
+    print("Mine too!")
+else: 
+    print("nah my favorite animal is better")
