9e474f010c983572370c90141b21a3a585f700ad
jordancarron11 <jordancarron11@gmail.com>
2023-01-21 15:07:13 -0700

    add

---
+++ Checkpoint07.py
@@ -0,0 +1,11 @@
+x = int(input("Please type a positive number: "))
+
+while x <= 0:
+    print("Sorry, that is a negative number. Please try again.")
+    x = int(input("Please type a positive number: "))
+print("Thank you for the positive vibes dawg!")
+
+answer = input("May I have a piece of candy?: ")
+while answer == "no":
+    answer = input("May I have a piece of candy?: ")
+print("thank you for the candy dawg!")
