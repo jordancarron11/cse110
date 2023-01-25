dd54520352bd4ef8fabdee42ee79f82af86101a5
jordancarron11 <jordancarron11@gmail.com>
2023-01-24 18:06:23 -0700

    add

---
+++ Proves/dictionary.py
@@ -0,0 +1,42 @@
+Five = [
+'world', 
+'house', 
+'place', 
+'group', 
+'party', 
+'money', 
+'point', 
+'smith', 
+'water', 
+'nephi', 
+'order', 
+'power', 
+'court', 
+'level', 
+'child', 
+'south', 
+'bible', 
+'woman', 
+'north', 
+'sense', 
+'faith', 
+'table'
+]
+
+Six = [
+'glazed', 
+'harder', 
+'phoned',
+'active',
+'acting',
+'height',
+'strong'
+]
+
+Seven = [
+'strange',
+'cowards',
+'verbose',
+'heights'
+
+]
